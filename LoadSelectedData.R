9f5511a32e54683f5d65c7fcbb1b13a5aa3b1837
Gonzalo Bailador <gonza_bp@hotmail.com>
2017-03-24 10:14:35 +0100

    Renaming file

--- LoadSelectedData.R
+++ LoadSelectedData.R
@@ -5,5 +5,3 @@ dateTime<-paste(data$Date,data$Time,sep=" ")
 data$TimeStamp<-strptime(dateTime,format="%d/%m/%Y %H:%M:%S")
 
 selectedData<-data[data$TimeStamp>=strptime("2007-02-01",format="%Y-%m-%d") & data$TimeStamp<=strptime("2007-02-02 23:59:59",format="%Y-%m-%d %H:%M:%S"),]
-
-write.csv(selectedData,file="selectedData.csv")
